facd7a7ef68f7987b188e8270ab98c2c81862d25
ValerieMbaka <valeriembaka0@gmail.com>
2025-03-18 12:36:14 +0300

    Arrays

--- src/Operator.java
+++ src/Operator.java
@@ -14,11 +14,37 @@ public class Operator {
                         names[x] = input.nextLine();
                 }
                 
-                System.out.println("Student marks are as below: ");
+                System.out.println("Student marks before sorting are as below: ");
                 for (short s:marks) {
                         System.out.print(s + ", ");
                 }
                 System.out.println();
+                
+                sortArray(marks);
+                System.out.println("Student marks after sorting in descending order are as below: ");
+                for (short mark : marks) {
+                        System.out.print(mark + ", ");
+                }
+        }
+        
+        public static void sortArray(short[] marks) {
+                int length = marks.length;
+                short temp;
+                boolean swapped;
+                
+                for (int i = 0; i < length - 1; i++) {
+                        swapped = false;
+                        
+                        for (int j = 0; j < length - i - 1; j++) {
+                                if (marks[j] < marks[j + 1]) { // Swap if next element is larger
+                                        temp = marks[j];
+                                        marks[j] = marks[j + 1];
+                                        marks[j + 1] = temp;
+                                        swapped = true;
+                                }
+                        }
+                }
+               
         }
         
         public static void sum(int ...numbers) {
@@ -41,10 +67,10 @@ public class Operator {
                 
                 // System.out.println("Rectangle Length is: " + r.getLength());
                 
-                // myArray();
+                myArray();
                 
-                sum(5, 3);
-                sum(8, 1, 4, 6, 7);
-                sum(20, 22, 71, 41, 32, 57);
+                // sum(5, 3);
+                // sum(8, 1, 4, 6, 7);
+                // sum(20, 22, 71, 41, 32, 57);
         }
 }
